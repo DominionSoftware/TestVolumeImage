2e0487992f64a03d7dc22f2d9879eae5558f2ec9
Rick Frank <rfrank@dominionsw.com>
2018-07-20 16:21:06 -0400

    check in configuration files.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var webpack = require('webpack');
+var vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.v2.rules;
+var entry = path.join(__dirname, './src/index.js');
+const sourcePath = path.join(__dirname, './src');
+const outputPath = path.join(__dirname, './dist');
+module.exports = {
+    entry,
+    output: {path: outputPath, filename: 'TestVolumeImage.js',},
+    module: {
+        rules: [{test: entry, loader: "expose-loader?TestVolumeImage"}, {
+            test: /\.html$/,
+            loader: 'html-loader'
+        },].concat(vtkRules),
+    },
+    resolve: {modules: [path.resolve(__dirname, 'node_modules'), sourcePath,],},
+};
+
